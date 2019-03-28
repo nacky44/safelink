2895c96ba1c0ac34932ab17343c12cdc159a49a6
nacky44 <48365119+nacky44@users.noreply.github.com>
2019-03-29 01:04:52 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -121,7 +121,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "https://www.panduanblog.online/p/go_to_url.html?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://www.dtgaming.xyz/p/safelink_29.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
